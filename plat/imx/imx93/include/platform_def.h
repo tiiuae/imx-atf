3e10cf7446558606d76e880bc926b42e9a7b7980
Theodore Karatapanis <tkaratapanis@census-labs.com>
2025-06-11 16:17:06 +0300

    saluki-nxp93: Pin the FDT address
    Signed-off-by: Theodore Karatapanis <tkaratapanis@census-labs.com>

--- plat/imx/imx93/include/platform_def.h
+++ plat/imx/imx93/include/platform_def.h
@@ -36,7 +36,9 @@
 /* non-secure uboot base */
 /* TODO */
 #define PLAT_NS_IMAGE_OFFSET		U(0x80010000)
-#define BL32_FDT_OVERLAY_ADDR           (PLAT_NS_IMAGE_OFFSET + 0x3000000)
+
+/* Unused currently. */
+#define BL32_FDT_OVERLAY_ADDR           (0x88200000)
 
 /* GICv4 base address */
 #define PLAT_GICD_BASE			U(0x48000000)
